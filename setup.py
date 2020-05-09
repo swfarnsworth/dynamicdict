a07f2c727636bf68bb324a8739fddea8b51af6f6
swfarnsworth <swfarnsworth@gmail.com>
2020-05-09 14:18:25 -0400

    Added to setup call

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from distutils.core import setup, Extension
 
 
+with open('./README.md') as f:
+    long_description = f.read()
+
+
 def main():
     setup(
-        name="dynamicdict",
-        version="1.0.0",
-        description="TODO",
-        author="Steele Farnsworth",
-        author_email="swfarnsworth@gmail.com",
-        ext_modules=[Extension("dynamicdict", ["dynamicdict.c"])]
+        name='dynamicdict',
+        version='0.0.1',
+        description='Dict-like type that creates values for missing keys in terms of the ke',
+        long_description=long_description,
+        author='Steele Farnsworth',
+        author_email='swfarnsworth@gmail.com',
+        url='https://github.com/swfarnsworth/dynamicdict',
+        ext_modules=[Extension('dynamicdict', ['dynamicdict.c'])]
     )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
